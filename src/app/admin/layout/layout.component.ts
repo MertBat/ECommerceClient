3278f519b9717ba26414b09acfe93461ebf21c16
MertBat <mrt01bb@gmail.com>
2023-11-27 00:04:31 +0300

    alertJs added

---
+++ src/app/admin/layout/layout.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+})
+export class LayoutComponent implements OnInit {
+
+  constructor(private alertify: AlertifyService) {}
+
+  ngOnInit(): void {
+    this.alertify.message("mert",{
+      messageType: MessageType.Success,
+      delay: 5,
+      position: Position.TopCenter
+    });
+    // this.alertify.dismiss();
+  }
+   b(){
+    this.alertify.dismiss();
+
+  }
+}
