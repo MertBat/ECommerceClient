f90e085c60a4cf1d87f8bd9d3cee4a4005095b4a
MertBat <mrt01bb@gmail.com>
2024-03-20 18:36:37 +0300

    Fixing admin pannel layout

--- src/app/admin/components/components.module.ts
+++ src/app/admin/components/components.module.ts
@@ -1,22 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductsModule } from './products/products.module';
-import { OrderModule } from './order/order.module';
-import { DashboardModule } from './dashboard/dashboard.module';
+import { NgModule } from '@angular/core';
+import { AuthorizeMenuModule } from './authorize-menu/authorize-menu.module';
 import { CustomerModule } from './customer/customer.module';
-
-
+import { DashboardModule } from './dashboard/dashboard.module';
+import { OrderModule } from './order/order.module';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
-  declarations: [
-
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     ProductsModule,
     OrderModule,
     DashboardModule,
-    CustomerModule
-  ]
+    CustomerModule,
+    AuthorizeMenuModule
+  ],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
