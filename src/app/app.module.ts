bdee3adec97a57c6d559ede9bb5a4d66336590ef
MertBat <mrt01bb@gmail.com>
2024-04-26 18:04:15 +0300

    Urls fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,8 +47,8 @@ import {MatIconModule} from '@angular/material/icon';
     SocialLoginModule
   ],
   providers: [
-    {provide: "baseUrl", useValue: "http://ecommerce-env.eba-umzzrvqq.us-east-1.elasticbeanstalk.com/api", multi:true},
-    {provide: "baseSignalRUrl", useValue: "http://ecommerce-env.eba-umzzrvqq.us-east-1.elasticbeanstalk.com/", multi:true},
+    {provide: "baseUrl", useValue: "www.ecommerce-env.eba-umzzrvqq.us-east-1.elasticbeanstalk.com/api", multi:true},
+    {provide: "baseSignalRUrl", useValue: "www.ecommerce-env.eba-umzzrvqq.us-east-1.elasticbeanstalk.com/", multi:true},
     {
       provide: 'SocialAuthServiceConfig',
       useValue: {
